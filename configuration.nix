14ee37cdf428a670f5ae4e846087cbfbf34de96a
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-10 15:07:53 +0000

    feat(config): set EDITOR/VISUAL environment variables

--- configuration.nix
+++ configuration.nix
@@ -5,11 +5,17 @@
   ...
 }: {
   nix.settings.experimental-features = ["nix-command" "flakes"];
+
   environment.systemPackages = with pkgs; [
     git
     lazygit
     neovim
   ];
+
+  environment.variables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
 
   wsl.defaultUser = "drop-stones";
 }
