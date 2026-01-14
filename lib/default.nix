4b9627905e57a7038fee05d1608c753aad423453
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-14 12:43:47 +0000

    feat(lib): fromRoot build a path by appending subpath to root

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,7 @@
-{ lib }:
+{ lib, root }:
 {
   nixosSystem = import ./nixosSystem.nix;
 
+  fromRoot = import ./fromRoot.nix { inherit lib root; };
   listImports = import ./listImports.nix { inherit lib; };
 }
---
+++ lib/fromRoot.nix
@@ -0,0 +1,7 @@
+# fromRoot :: { lib: attrs, root: string } -> string -> path
+#
+# Build a path by appending a relative subpath to the flake root.
+# - root is expected to be self.outPath (a string, usually with string context)
+# - we discard string context to avoid accidental dependency tracking issues
+# - subpath must be a relative string without a leading slash
+{ lib, root }: subpath: lib.path.append (/. + builtins.unsafeDiscardStringContext root) subpath
