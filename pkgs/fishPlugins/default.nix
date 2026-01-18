40c52febc22d545d30123c7adf59dedac6c2e732
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-18 13:18:06 +0000

    feat(pkgs): add pkgs.fishPlugins

---
+++ pkgs/fishPlugins/default.nix
@@ -0,0 +1,3 @@
+# Collect fish plugins under ./pkgs/fishPlugins and return them as an attrset.
+{ prev, localLib, ... } @ args:
+localLib.importDir { dir = ./.; mkValue = (p: prev.callPackage p args); }
