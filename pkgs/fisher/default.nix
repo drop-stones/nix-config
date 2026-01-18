025efb5ed113328cad45f731bafe679b3a576dd5
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-18 06:33:58 +0000

    feat(pkgs): manage fisher as a private package

---
+++ pkgs/fisher/default.nix
@@ -0,0 +1,26 @@
+{ lib, stdenvNoCC, localLib, fisher, ... }:
+stdenvNoCC.mkDerivation {
+  pname = "fisher";
+  version = localLib.getInputVersionFromLock { name = "fisher"; };
+
+  src = fisher;
+
+  dontBuild = true;
+  dontConfigure = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    install -D --mode=644 functions/fisher.fish $out/share/fish/vendor_functions.d/fisher.fish
+    install -D --mode=644 completions/fisher.fish $out/share/fish/vendor_completions.d/fisher.fish
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "A plugin manager for Fish";
+    homepage = "https://github.com/jorgebucaran/fisher";
+    license = licenses.mit;
+    platforms = platforms.all;
+  };
+}
