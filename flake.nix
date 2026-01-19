bd2526c7482d7d94ff9a251eec9f2ced99a86bb2
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-19 12:58:25 +0000

    feat(flake): update nixpkgs version

--- flake.nix
+++ flake.nix
@@ -2,10 +2,13 @@
   description = "drop-stones' nix configuration for NixOS/macOS/WSL2";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixos-wsl.url = "github:nix-community/NixOS-WSL/release-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs"; # same version with inputs.nixpkgs
     };
 
