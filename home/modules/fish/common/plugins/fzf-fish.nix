4f7cf4cc2e7af87253f64cf610f34d8a35685a8c
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-21 11:15:05 +0000

    feat(fzf-fish): configure keybindings

--- home/modules/fish/common/plugins/fzf-fish.nix
+++ home/modules/fish/common/plugins/fzf-fish.nix
@@ -1,4 +1,21 @@
-{ pkgs, localLib, ... }:
-{
-  programs.fish.plugins = [ (localLib.mkPlugin pkgs.fishPlugins.fzf-fish) ];
+{ pkgs, config, lib, localLib, ... }:
+lib.mkIf (config.programs.fzf.enable) {
+  programs.fish = {
+    # install fzf-fish
+    plugins = [ (localLib.mkPlugin pkgs.fishPlugins.fzf-fish) ];
+
+    # fzf-fish settings
+    interactiveShellInit = ''
+      fzf_configure_bindings \
+        --history=\cr \
+        --directory= \
+        --git_log= \
+        --git_status= \
+        --processes= \
+        --variables=
+    '';
+  };
+
+  # disable fzf integration
+  programs.fzf.enableFishIntegration = false;
 }
