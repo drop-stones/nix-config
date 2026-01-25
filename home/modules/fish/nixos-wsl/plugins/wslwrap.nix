4a1270ba527c6a24edb24d873c670c62e68264e2
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-25 13:42:50 +0000

    feat(wslwrap.fish): register/link settings

--- home/modules/fish/nixos-wsl/plugins/wslwrap.nix
+++ home/modules/fish/nixos-wsl/plugins/wslwrap.nix
@@ -1,4 +1,34 @@
 { pkgs, localLib, ... }:
 {
-  programs.fish.plugins = [ (localLib.mkPlugin pkgs.fishPlugins.wslwrap) ];
+  programs.fish = {
+    # install wslwrap.fish
+    plugins = [ (localLib.mkPlugin pkgs.fishPlugins.wslwrap) ];
+
+    # settings
+    interactiveShellInit = ''
+      # wslwrap.fish
+
+      ## auto
+      wslwrap register bat
+      wslwrap register eza
+      wslwrap register fd --path-separator=/
+      wslwrap register git
+      wslwrap register lazygit
+      wslwrap register nvim
+      wslwrap register rg
+
+      ## windows
+      wslwrap register cmd --mode windows
+      wslwrap register explorer --mode windows
+      wslwrap register powershell --mode windows
+      wslwrap register pwsh --mode windows
+      wslwrap register wsl --mode windows
+
+      ## symlink
+      wslwrap link cmd.exe
+      wslwrap link powershell.exe
+      wslwrap link pwsh.exe
+      wslwrap link win32yank.exe
+    '';
+  };
 }
