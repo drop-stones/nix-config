179e1a395be8c39e1592def3f5ed4fcfba3f45a8
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-26 11:43:33 +0000

    feat(nixos-wsl): set WSLENV

---
+++ home/nixos-wsl/env.nix
@@ -0,0 +1,12 @@
+{ lib, ... }:
+{
+  home.sessionVariables = {
+    WSLENV = (lib.concatStringsSep ":" [
+      "LS_COLORS/w"
+      "WSL_DISTRO_NAME/w"
+      "ZELLIJ/w"
+      "ZELLIJ_SESSION_NAME/w"
+      "ZELLIJ_PANE_ID/w"
+    ]);
+  };
+}
