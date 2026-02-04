3db6c14e77fd393a7cd8662eba8edcd67b466ad6
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-04 14:17:12 +0000

    feat(user): add git data to drop-stones

--- data/users/drop-stones.nix
+++ data/users/drop-stones.nix
@@ -1,4 +1,19 @@
 {
+  # Unix account
   username = "drop-stones";
   homeDirectory = "/home/drop-stones";
+
+  git = {
+    # Git identity
+    name = "drop-stones";
+    email = "s2h6i8z1u7k8u7@gmail.com";
+
+    # Git commit signing (SSH via 1password)
+    gpgsign = {
+      enable = true;
+      signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOlftWsuO8svvievtqfCj6civRTSU+DCqThc23AGbWtp";
+      format = "ssh";
+      program = "op-ssh-sign-wsl.exe";
+    };
+  };
 }
