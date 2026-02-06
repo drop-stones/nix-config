c150c2bf7dde8ab09c6019560ceb8dbf3a3de35e
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-06 15:21:27 +0000

    feat(secrets): symlink ~/.config/git/config

--- secrets/default.nix
+++ secrets/default.nix
@@ -1,6 +1,8 @@
 {
   agenix,
+  secrets-config,
   system,
+  user,
   ...
 }:
 {
@@ -11,4 +13,13 @@
 
   # decryption key
   age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+  age.secrets."gitconfig" = {
+    symlink = true;
+    path = "${user.homeDirectory}/.config/git/config";
+    file = "${secrets-config}/git/config.age";
+    owner = "${user.username}";
+    group = "users";
+    mode = "0400";
+  };
 }
