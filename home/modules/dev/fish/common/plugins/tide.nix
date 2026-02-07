b125181d11dae487f74d23f4ca417e464b1fd800
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-07 01:01:48 +0000

    feat(tide): configure tide after `nixos-rebuild switch`

--- home/modules/dev/fish/common/plugins/tide.nix
+++ home/modules/dev/fish/common/plugins/tide.nix
@@ -1,4 +1,30 @@
-{ pkgs, localLib, ... }:
 {
+  pkgs,
+  lib,
+  localLib,
+  ...
+}:
+let
+  tideArgs = lib.concatStringsSep " " [
+    "--auto"
+    "--style=Lean"
+    "--prompt_colors='True color'"
+    "--show_time=No"
+    "--lean_prompt_height='Two lines'"
+    "--prompt_connection=Disconnected"
+    "--prompt_spacing=Sparse"
+    "--icons='Few icons'"
+    "--transient=Yes"
+  ];
+in
+{
+  # tide: 🌊 The ultimate Fish prompt
   programs.fish.plugins = [ (localLib.mkPlugin pkgs.fishPlugins.tide) ];
+
+  # configure tide after `nixos-rebuild switch`
+  home.activation = {
+    tideConfigure = lib.hm.dag.entryAfter [ "installPackages" ] ''
+      ${pkgs.fish}/bin/fish -lc "tide configure ${tideArgs}" 2>&1 >/dev/null
+    '';
+  };
 }
