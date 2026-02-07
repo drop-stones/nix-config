0ea7a3fa907e5007746c06747acdee6f494ae1a6
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-07 16:43:53 +0900

    feat(lib): build a darwin system configuration

---
+++ lib/darwinSystem.nix
@@ -0,0 +1,37 @@
+# darwinSystem :: {
+#   user: { username: string, ... },
+#   system: string,
+#   darwin-modules: [ module ],
+#   home-modules: [ module ],
+#   specialArgs: attrs,
+# } -> darwinSystem
+#
+# Build a darwin system configuration and integrate Home Manager:
+# - appends home-manager.darwinModules.home-manager to the darwin module list
+# - reuses the same pkgs set between darwin and Home Manager
+# - forwards specialArgs to Home Manager via extraSpecialArgs
+# - imports the given Home Manager modules for the specified user
+{ lib, home-manager, nix-darwin, ... }:
+{
+  user,
+  system,
+  darwin-modules,
+  home-modules,
+  specialArgs,
+  ...
+}:
+nix-darwin.lib.darwinSystem {
+  inherit system specialArgs;
+
+  modules = darwin-modules ++ [
+    home-manager.darwinModules.home-manager
+    {
+      home-manager.useGlobalPkgs = true;
+      home-manager.useUserPackages = true;
+
+      home-manager.extraSpecialArgs = specialArgs;
+
+      home-manager.users.${user.username}.imports = home-modules;
+    }
+  ];
+}
