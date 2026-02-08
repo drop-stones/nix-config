df2dbd1fa9577f5df7289dce6459b6e6c5127846
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-08 04:32:52 +0000

    feat(initcache): disable zellij setup on nixos-wsl (very buggy)

--- home/modules/dev/fish/darwin/plugins/initcache.fish/zellij.nix
+++ home/modules/dev/fish/darwin/plugins/initcache.fish/zellij.nix
@@ -4,8 +4,4 @@
   programs.fish.interactiveShellInit = ''
     ${lib.optionalString config.programs.zellij.enable "initcache source -- zellij setup --generate-auto-start fish"}
   '';
-
-  # disable zellij integration if zellij is enabled
-  programs.zellij.enableFishIntegration =
-    if (config.programs.zellij.enable) then false else config.programs.zellij.enableFishIntegration;
 }
