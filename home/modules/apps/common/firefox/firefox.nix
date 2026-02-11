e1b70ca0ac08b341781e03bdea70bf5c030f3b46
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-11 15:23:27 +0900

    feat(firefox): Web browser from Mozilla organization

---
+++ home/modules/apps/common/firefox/firefox.nix
@@ -0,0 +1,4 @@
+_: {
+  # Firefox Browser: A fast, reliable and private web browser from the non-profit Mozilla organization
+  programs.firefox.enable = true;
+}
---
+++ home/modules/apps/common/firefox/languages.nix
@@ -0,0 +1,6 @@
+_: {
+  programs.firefox.languagePacks = [
+    "en-US"
+    "ja"
+  ];
+}
