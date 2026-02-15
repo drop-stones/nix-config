e182636a48c32e55b23b0114ac7ed2c81a9df8bf
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-15 07:10:00 +0000

    feat(secrets): add fish-config

--- secrets/default.nix
+++ secrets/default.nix
@@ -14,12 +14,22 @@
   # decryption key
   age.identityPaths = [ "${data.user.homeDirectory}/.local/share/age/key.txt" ];
 
-  age.secrets."gitconfig" = {
-    symlink = true;
-    path = "${data.user.homeDirectory}/.config/git/config";
-    file = "${secrets-config}/git/config.age";
-    owner = "${data.user.username}";
-    group = "users";
-    mode = "0400";
+  age.secrets = {
+    "gitconfig" = {
+      symlink = true;
+      path = "${data.user.homeDirectory}/.config/git/config";
+      file = "${secrets-config}/nixos-wsl/git/config.age";
+      owner = "${data.user.username}";
+      group = "users";
+      mode = "0400";
+    };
+    "fish-config" = {
+      symlink = true;
+      path = "${data.user.homeDirectory}/.config/fish/conf.d/zzz-local.fish"; # load after plugin loading
+      file = "${secrets-config}/nixos-wsl/fish/conf.d/local.fish.age";
+      owner = "${data.user.username}";
+      group = "users";
+      mode = "0400";
+    };
   };
 }
