712e16a7f979512caedcc721dff06ba5e0f29b0f
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-21 08:43:46 +0000

    feat(fcitx5): Next generation of fcitx, cross-platform input method framework

---
+++ home/modules/desktop/nixos/fcitx5/profile.nix
@@ -0,0 +1,20 @@
+_: {
+  i18n.inputMethod.fcitx5.settings.inputMethod = {
+    GroupOrder = {
+      "0" = "Default";
+    };
+    "Groups/0" = {
+      Name = "Default";
+      "Default Layout" = "us";
+      DefaultIM = "hazkey";
+    };
+    "Groups/0/Items/0" = {
+      Name = "keyboard-us";
+      Layout = "";
+    };
+    "Groups/0/Items/1" = {
+      Name = "hazkey";
+      Layout = "";
+    };
+  };
+}
