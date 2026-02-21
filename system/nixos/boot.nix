7aaad9c54e0984695d0ee3935905ab0ab2aceed7
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-02-21 12:23:08 +0900

    feat(boot): Use the latest Linux kernel

--- system/nixos/boot.nix
+++ system/nixos/boot.nix
@@ -1,5 +1,11 @@
-_: {
-  # systemd-boot: An easy-to-configure UEFI boot manager
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+{ pkgs, ... }:
+{
+  boot = {
+    # systemd-boot: An easy-to-configure UEFI boot manager
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+
+    # use latest linux kernel
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
 }
