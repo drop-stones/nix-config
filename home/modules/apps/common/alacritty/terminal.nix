ab05016f664db8b97422da44b3bae5a26057ba6e
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-03-15 22:25:13 +0900

    feat(zellij): launch "main" session from alacritty

--- home/modules/apps/common/alacritty/terminal.nix
+++ home/modules/apps/common/alacritty/terminal.nix
@@ -1,10 +1,11 @@
-{ pkgs, ... }:
-{
+_: {
   programs.alacritty.settings = {
     terminal.shell = {
-      # program = "${pkgs.tmux}/bin/tmux";
-      # program = "${pkgs.zellij}/bin/zellij";
-      program = "${pkgs.fish}/bin/fish";
+      program = "/run/current-system/sw/bin/fish";
+      args = [
+        "-c"
+        "zellij"
+      ];
     };
   };
 }
