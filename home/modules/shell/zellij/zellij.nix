658c757168b03ea92ceaed170d1ec905a0d4934a
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-04-07 18:23:50 +0900

    feat(zellij): use pkgs-unstable package for zellij

--- home/modules/shell/zellij/zellij.nix
+++ home/modules/shell/zellij/zellij.nix
@@ -1,25 +1,8 @@
-# Temporary: build zellij v0.44.0 with unstable toolchain
-# Remove once nixpkgs-unstable ships v0.44.0
-{ pkgs, pkgs-unstable, ... }:
-let
-  version = "0.44.0";
-  src = pkgs.fetchFromGitHub {
-    owner = "zellij-org";
-    repo = "zellij";
-    tag = "v${version}";
-    hash = "sha256-9jVBz+in8TrdX5qYXdSBCN3SYhG/JJLIbkq0DC0tm4Q=";
-  };
-in
+{ pkgs-unstable, ... }:
 {
+  # zellij: A terminal workspace with batteries included
   programs.zellij = {
     enable = true;
-    package = pkgs-unstable.zellij.overrideAttrs (old: {
-      inherit version src;
-      cargoDeps = pkgs-unstable.rustPlatform.fetchCargoVendor {
-        inherit src;
-        name = "zellij-${version}";
-        hash = "sha256-nGMOVq5etxiOfocjTKXAd8sJHFw34T49Ga48Isc8dCg=";
-      };
-    });
+    package = pkgs-unstable.zellij;
   };
 }
